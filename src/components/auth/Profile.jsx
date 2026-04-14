5487f1533e23a57070c8e22102006b882cf43b58
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-04-14 13:26:07 +0100

    fetch user data

--- src/components/auth/Profile.jsx
+++ src/components/auth/Profile.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
-
+import React,{useState,useEffect} from 'react'
+import { UserAPI } from '../../api/api';
 const Profile = () => {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await UserAPI.user_data();
+        setUserData(response.data);
+        console.log(response.data)
+      } catch (error) {
+        console.error('Failed to fetch user data', error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+
   return (
     <div>Profile</div>
   )
